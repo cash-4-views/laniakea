63a6368dfa36ffa215dd271e65f4e0b676a53f53
James <james.griffiths.2222@googlemail.com>
2015-04-14 18:58:49 +0100

    tings

--- api/server.js
+++ api/server.js
@@ -1,24 +1,21 @@
-var Hapi = require('hapi');
-var fs = require("fs");
-var csvParser = require('./utils/csvParser');
-var Path = require('path');
+var Hapi  	  = require('hapi'),
+	fs   	  = require("fs"),
+	Path 	  = require('path'),
+	csvParser = require('./utils/csvParser');
 
-
-// Create a server instance
 var server = new Hapi.Server();
 
-// Create a connection which will listen on port 8000
 server.connection({
     host: 'localhost',
     port: 8000
 });
 
-// Add a GET endpoint /hello
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
     	var file = Path.resolve(__dirname, './sampledata/report.csv');
+
     	fs.readFile(file, function(err, contents){
     		if (err){
     			return console.log(err);

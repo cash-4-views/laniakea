dab2f5fdcbf6ec6055eee3768da73037c405f70b
James <james.griffiths.2222@googlemail.com>
2015-04-14 18:18:56 +0100

    oh shit im on development - fixed csvParser text

--- test/__server.js
+++ test/__server.js
@@ -4,13 +4,14 @@ var server  = require("../api/server.js");
 
 Test("/, unauthenticated", function(t) {
 
+	"use strict";
+
 	var opts = {
 		url	   : "/",
 		method : "GET"
 	};
 
 	Shot.inject(server, opts, function(res) {
-		console.log(res.statusCode);
 		t.plan(1);
 		t.assert(res.statusCode, 200, "returns a 200 status code");
 	});

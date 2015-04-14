12b4e35a5d234fdee7077b17b4a2a33a52cc4d38
beechware <davebeechmovement@yahoo.co.uk>
2015-04-14 17:52:07 +0100

    fixed server resolve

--- api/server.js
+++ api/server.js
@@ -1,5 +1,35 @@
-var Hapi = require("hapi");
+var Hapi = require('hapi');
+var fs = require("fs");
+var csvParser = require('./utils/csvParser');
+var Path = require('path');
 
+
+ 
+// Create a server instance
 var server = new Hapi.Server();
+ 
+// Create a connection which will listen on port 8000
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+ 
+// Add a GET endpoint /hello
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+    	var file = Path.resolve(__dirname, './sampledata/report.csv');
+    	fs.readFile(file, function(err, contents){
+    		if (err){
+    			return console.log(err);
+    		} else {
+    			reply(csvParser(contents));
+    		}
 
-module.exports = server;
+    	});
+    	
+    }
+ });
+
+ module.exports = server;

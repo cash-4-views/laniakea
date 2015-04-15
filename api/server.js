c0ea7f36a306a70959fd8c2854c94f62403dbc95
James <james.griffiths.2222@googlemail.com>
2015-04-15 12:57:24 +0100

    minors, basic form, tests

--- api/server.js
+++ api/server.js
@@ -1,35 +1,64 @@
-var Hapi  	  = require('hapi'),
-	fs   	  = require("fs"),
-	Path 	  = require('path'),
-	csvParser = require('./utils/csvParser');
+var Hapi  	  = require("hapi"),
+		fs   	  	= require("fs"),
+		Path 	  	= require("path"),
+		csvParser = require("./utils/csvParser");
 
 var server = new Hapi.Server();
 
 server.connection({
-		host: 'localhost',
-		port: 8000
+	port: process.env.PORT || 8000
+});
+
+server.views({
+
+	engines: {
+		jade: require("jade")
+	},
+
+	compileOptions: {
+		pretty: true
+	},
+
+	relativeTo: __dirname,
+	path: 		  "./views",
+	isCached: false
+
 });
 
 server.route([
-	{
-		path: '/',
-		method: 'GET',
-		handler: function (request, reply) {
-			var file = Path.resolve(__dirname, './sampledata/report.csv');
 
-			fs.readFile(file, function(err, contents){
-				if (err){
-					return console.log(err);
-				} else {
-					reply(csvParser(contents));
-				}
-			});
-		}
-	},
 	{
 		path: "/",
-		method: "PUT",
+		method: "GET",
+		handler: function (req, reply) {
+			reply.view("login");
+		}
+	},
+
+	{
+		path: "/login",
+		method: "POST",
+		handler: function(req, reply) {
+			return;
+		}
+	},
+
+	{
+		path: "/signup",
+		method: "POST",
+		handler: function(req, reply) {
+
+		}
+	},
+
+	{
+		path: "/logout",
+		method: "GET",
+		handler: function(req, reply) {
+
+		}
 	}
+
 ]);
 
  module.exports = server;

e4ef69593170c6251a0219e40d9b6ed37ffa3250
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-04-22 13:56:25 +0100

    main.js edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 var React = require("react");
+var superagent = require("superagent");
 var ReportData = require("./components/ReportData");
+
+request
+	.get("/api/v1/reports/{2015_01}")
+	.end(function(err, res) {
+		"use strict";
+		if (err) console.log("AJAX error: " + err);
+		console.log(res.body);
+	});
+
 
 React.render(
 	<ReportData />, document.getElementById("content")

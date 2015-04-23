cd5cf60cde1bde8f933c57abc0bca57c3198910c
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-04-23 10:54:33 +0100

    main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 var React 		 = require("react");
 var request 	 = require("superagent");
 
-var ReportApp = require("./components/ReportApp");
+var ReportData = require("./components/ReportData");
 
 var dataArray;
 
@@ -10,11 +10,11 @@ var ReportPage = React.createClass({
 	ajaxCall : function() {
 		"use strict";
 		request
-			.get("api/v1/reports/2015_01/Royal_Albert_Hall")
+			.get("/../api/v1/reports/2015_01")
 			.end(function(err, res) {
 				if (err) console.log("AJAX error: " + err);
 				console.log("ajax call finished");
-				this.setState({data : res.body[0]});
+				this.setState({data : res.body});
 				console.log("state set");
 				console.log(this.state.data);
 			}.bind(this));

09f2f532eb32fbae7fe90185ff2dbacbe972733c
James <james.griffiths.2222@googlemail.com>
2015-04-24 01:26:56 +0100

    more

--- src/js/components/ReportAlert.js
+++ src/js/components/ReportAlert.js
@@ -7,12 +7,12 @@ var ReportAlert = React.createClass({
 		"use strict";
 
 		var closeIcon = <span aria-hidden="true">&times;</span>;
-
+		var color 		= this.props.alert.type === "Success!" ? "success" : "danger";
 		return(
-			<div className="alert alert-success alert-dismissible" role="alert">
+			<div className={"alert alert-" + color + " alert-dismissible"} role="alert">
 				<Button size="small close" isDisabled={false} onClickCallback={this.props.closeAlert} content={closeIcon}/>
-			  <strong>{this.props.alert.type}</strong>
-			  {" " + this.props.alert.content}
+			  <strong>{this.props.alert.type + " "}</strong>
+			  {this.props.alert.content}
 			</div>
 			);
 	}

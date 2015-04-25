f7f95adb9b5cae193dd346cfcbf33f9b883e5b1b
James <james.griffiths.2222@googlemail.com>
2015-04-25 14:18:25 +0100

    tests

--- test/__Report.js
+++ test/__Report.js
@@ -7,75 +7,75 @@ var tableSvc 		 = azure.createTableService(config.dbacc, config.dbkey),
 		tableName 	 = config.rtable + Date.now(),
 		report;
 
-test("Preparation", function(t) {
-	"use strict";
+// test("Preparation", function(t) {
+// 	"use strict";
 
-	tableSvc.doesTableExist(tableName, function(errPing, res) {
-		if(res) {
-			t.comment("Table already exists. Deleting table...");
-			tableSvc.deleteTableIfExists(tableName, function(errDel, result) {
-				if(result) setTimeout(function() {return;}, 5000);
-			});
-		}
+// 	tableSvc.doesTableExist(tableName, function(errPing, res) {
+// 		if(res) {
+// 			t.comment("Table already exists. Deleting table...");
+// 			tableSvc.deleteTableIfExists(tableName, function(errDel, result) {
+// 				if(result) setTimeout(function() {return;}, 5000);
+// 			});
+// 		}
 
-		report = new Report(tableSvc, tableName);
-		setTimeout(t.end, 500);
-	});
+// 		report = new Report(tableSvc, tableName);
+// 		setTimeout(t.end, 500);
+// 	});
 
-});
+// });
 
-test("The getReport function", function(t) {
-	"use strict";
+// test("The getReport function", function(t) {
+// 	"use strict";
 
-	// report.getReport("2016_01", "Terry_Teriyaki", false, false, function(err, results, continuationToken) {
-	// 	t.notOk(err, "should not return an error");
-		t.end();
-	// });
-});
+// 	// report.getReport("2016_01", "Terry_Teriyaki", false, false, function(err, results, continuationToken) {
+// 	// 	t.notOk(err, "should not return an error");
+// 		t.end();
+// 	// });
+// });
 
-test("The getNextBatch function", function(t) {
-	"use strict";
+// test("The getNextBatch function", function(t) {
+// 	"use strict";
 
-	t.end();
-});
+// 	t.end();
+// });
 
-test("The createReport function", function(t) {
-	"use strict";
+// test("The createReport function", function(t) {
+// 	"use strict";
 
-	t.end();
-});
+// 	t.end();
+// });
 
-test("The createReportRow function", function(t) {
-	"use strict";
+// test("The createReportRow function", function(t) {
+// 	"use strict";
 
-	t.end();
-});
-
-
-test("The updateReportRow function", function(t) {
-	"use strict";
-
-	t.end();
-});
-
-test("The getCustomIDList function", function(t) {
-	"use strict";
-
-	t.end();
-});
+// 	t.end();
+// });
 
 
-test("The approveAllOfCustomID function", function(t) {
-	"use strict";
+// test("The updateReportRow function", function(t) {
+// 	"use strict";
 
-	t.end();
-});
+// 	t.end();
+// });
 
-test("Cleaning up after ourselves - deleting the table, ", function(t) {
-	"use strict";
+// test("The getCustomIDList function", function(t) {
+// 	"use strict";
 
-	tableSvc.deleteTable(tableName, function(err) {
-		t.notOk(err, "should successfully delete the table");
-		t.end();
-	});
-});
+// 	t.end();
+// });
+
+
+// test("The approveAllOfCustomID function", function(t) {
+// 	"use strict";
+
+// 	t.end();
+// });
+
+// test("Cleaning up after ourselves - deleting the table, ", function(t) {
+// 	"use strict";
+
+// 	tableSvc.deleteTable(tableName, function(err) {
+// 		t.notOk(err, "should successfully delete the table");
+// 		t.end();
+// 	});
+// });

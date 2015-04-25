d667ca9fac5ca680b9b23a7110aa6ed4569b24bb
James <james.griffiths.2222@googlemail.com>
2015-04-25 10:54:37 +0100

    acc tests

--- test/__Account.js
+++ test/__Account.js
@@ -11,10 +11,14 @@ var tableSvc 		 = azure.createTableService(config.dbacc, config.dbkey),
 test("The Account constructer ", function(t) {
 	"use strict";
 
-	t.equal(approvedList.storageClient, tableSvc, "should return an object with the storage client we specified");
-	t.equal(approvedlist.tableName, "ACCOUNTTESTING", "should return an object with the table name specified");
-	t.equal(approvedlist.PartitionKey, "users", "should return an object with the partitionkey as our default value");
-	t.end();
+	t.equal(account.storageClient, tableSvc, "should return an object with the storage client we specified");
+	t.equal(account.tableName, "ACCOUNTTESTING", "should return an object with the table name specified");
+	t.equal(account.partitionKey, "users", "should return an object with the partitionkey as our default value");
+
+	tableSvc.createTable(tableName, function(err) {
+		t.ok(err, "should return an error as the table has already been created");
+		t.end();
+	});
 });
 
 test("The getAccounts function", function(t) {
@@ -43,19 +47,20 @@ test("The getAccounts function", function(t) {
 	var arrayOfAccountsWeWant = [];
 
 	tableSvc.insertEntity(tableName, newAccount2, {echoContent: true}, function(err1, result1, response2) {
-		t.notOk(error, "shouldn't throw an error writing account 1");
+		t.notOk(err1, "shouldn't throw an error writing account 1");
 		arrayOfAccountsWeWant.push(result1);
 
 		tableSvc.insertEntity(tableName, newAccount2, {echoContent: true}, function(err2, result2, response2) {
-			t.notOk(error, "shouldn't throw an error writing account 2");
+			t.notOk(err2, "shouldn't throw an error writing account 2");
 			arrayOfAccountsWeWant.push(result2);
 
 			account.getAccounts(function(err, accountsReturned) {
 				t.notOk(err, "shouldn't throw an error getting accounts");
-				t.deepEqual(accountsReturned, arrayOfAccountsWeWant, "should return an array of the same objects we put in")
+				t.deepEqual(accountsReturned, arrayOfAccountsWeWant, "should return an array of the same objects we put in");
 				t.end();
 			});
 
+		});
 	});
 });
 
@@ -72,15 +77,15 @@ test("The getSingleAccount function", function(t) {
 		admin 			: {_: "false"}
 	};
 
-	tableSvc.insertEntity(tableName, newAccount2, {echoContent: true}, function(err1, result1, response2) {
-		t.notOk(error, "shouldn't throw an error writing account 1");
+	tableSvc.insertEntity(tableName, newAccount1, {echoContent: true}, function(err1, result1, response2) {
+		t.notOk(err1, "shouldn't throw an error writing account 1");
 		var accountWeWant = result1;
 
 		t.plan(4);
 
 		account.getSingleAccount("TERRIBLE_TESTER@TIMMY.COM", function(err, accountReturned) {
 			t.notOk(err, "shouldn't throw an error getting account an existing account");
-			t.deepEqual(accountReturned, accountWeWant, "should return the account we asked for")
+			t.deepEqual(accountReturned, accountWeWant, "should return the account we asked for");
 		});
 
 		account.getSingleAccount("whoIsThis@nobody.com", function(err, accountReturned) {
@@ -100,20 +105,20 @@ test("The createSingleAccount function", function(t) {
 		email 			: "FOUNDERSANDCULTISTS@home.com",
 		phone 			: "06666666666",
 		admin 			: "false"
-	}
+	};
 
 	account.createSingleAccount(accountWeWant, function(err) {
 		t.notOk(err, "shouldn't return an error creating the account");
 
 		tableSvc.retrieveEntity(tableName, "users", "FOUNDERSANDCULTISTS@home.com", function(err, entity){
-			t.equal(entity.customid_, accountWeWant.customid, "should create the account with the provided customid");
-			t.equal(entity.password_, accountWeWant.password, "should create the account with the provided password");
+			t.equal(entity.customid_,  accountWeWant.customid, "should create the account with the provided customid");
+			t.equal(entity.password_,  accountWeWant.password, "should create the account with the provided password");
 			t.equal(entity.email_,		 accountWeWant.email,		 "should create the account with the provided email");
 			t.equal(entity.phone_,		 accountWeWant.phone,		 "should create the account with the provided phone");
 			t.equal(entity.admin_,		 accountWeWant.admin,		 "should create the account with the provided admin");
 			t.end();
-		})
-	})
+		});
+	});
 });
 
 test("The updateSingleAccount function", function(t) {

84fcf63950743e30b40b0340b2178b12061e342d
James <james.griffiths.2222@googlemail.com>
2015-04-25 19:36:33 +0100

    minor styling

--- src/js/components/report/ButtonTray.js
+++ src/js/components/report/ButtonTray.js
@@ -5,14 +5,24 @@ var ButtonTray = React.createClass({
 
 	render: function() {
 		"use strict";
+
+		var currentPanel = this.props.panel;
+
+		function activePanelClass(panel) {
+				console.log(currentPanel, panel)
+			if(currentPanel === panel) {
+				return "selected";
+			} else return "unselected";
+		}
+
 		return (
 			<div className="btn-group btn-group-justified tableSelector" role="group" aria-label="...">
-				  <Button type="success" size="chief" 	 value="approved"   content="Approved"   onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="approved"}/>
-				  <Button type="success" size="appended" value="Fetch All"  content="All" 		   onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="approved"}/>
-				  <Button type="info" 	 size="chief" 	 value="unapproved" content="Unapproved" onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="unapproved"}/>
-				  <Button type="info" 	 size="appended" value="Fetch All"  content="All"			   onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="unapproved"}/>
-				  <Button type="warning" size="chief" 	 value="unassigned" content="Unassigned" onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="unassigned"}/>
-				  <Button type="warning" size="appended" value="Fetch All"  content="All" 			 onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="unassigned"}/>
+				  <Button type={"success " + activePanelClass("approved")} 	size="chief" 	 value="approved"   content="Approved"   onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="approved"}/>
+				  <Button type="success" 																		size="appended" value="Fetch All"  content="All" 		   onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="approved"}/>
+				  <Button type={"info " + activePanelClass("unapproved")} 		size="chief" 	 value="unapproved" content="Unapproved" onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="unapproved"}/>
+				  <Button type="info" 																			size="appended" value="Fetch All"  content="All"			   onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="unapproved"}/>
+				  <Button type={"warning " + activePanelClass("unassigned")} size="chief" 	 value="unassigned" content="Unassigned" onClickCallback={this.props.switchReportPanel} isLoading={this.props.loadingPanel==="unassigned"}/>
+				  <Button type="warning" 																		size="appended" value="Fetch All"  content="All" 			 onClickCallback={this.props.getMoreResults} 		isDisabled={this.props.panel!=="unassigned"}/>
 			</div>
 			);
 	}
